9e803fe75a64b0c2a8247836aa96639671dbc9d2
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-07-15 12:23:30 -0400

    Heroes Service - traer data heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
   declarations: [AppComponent, ErrorPageComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ListadoComponent implements OnInit {
-
-  constructor() { }
+  constructor(private heroesService: HeroesService) {}
 
   ngOnInit(): void {
+    this.heroesService.getHeroes().subscribe((resp) => {
+      console.log(resp);
+    });
   }
-
 }
