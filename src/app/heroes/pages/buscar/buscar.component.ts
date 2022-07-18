a395ef490ca6304d68453ad5f5b3028a1de72515
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-07-18 11:36:16 -0400

    Autocomplete - segunda parte

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { Heroe } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
 
@@ -11,13 +12,24 @@ export class BuscarComponent implements OnInit {
   termino: string = '';
   heroes: Heroe[] = [];
 
+  heroeSeleccionado!: Heroe;
+
   constructor(private heroesService: HeroesService) {}
 
   ngOnInit(): void {}
 
   buscando() {
     this.heroesService
-      .getHeroes()
+      .getSugerencias(this.termino)
       .subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  opcionSeleccionada(event: MatAutocompleteSelectedEvent) {
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
+
+    this.heroesService
+      .getHeroePorId(heroe.id!)
+      .subscribe((heroe) => (this.heroeSeleccionado = heroe));
   }
 }
