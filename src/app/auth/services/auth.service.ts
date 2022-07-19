1fb7f74f5808b32939b55833afcb342e156b0714
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-07-19 12:40:45 -0400

    Mostrar la informacion del usuario activo get Auth()

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,16 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interface';
+import { tap } from 'rxjs';
 
 @Injectable({
    providedIn: 'root',
 })
 export class AuthService {
    private baseUrl: string = environment.baseUrl;
+   private _auth: Auth | undefined;
+
+   get auth(): Auth {
+      return { ...this._auth! };
+   }
 
    constructor(private http: HttpClient) {}
 
    login() {
-      return this.http.get<Auth>(`${this.baseUrl}/usuarios/2`);
+      return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`).pipe(
+         //efectos secundarios
+         tap((auth) => (this._auth = auth))
+      );
    }
 }
